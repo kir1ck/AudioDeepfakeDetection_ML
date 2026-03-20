a72433c0bcb6fed850e8492a632aba15d9a4c76a
kir1ck <kirill4557@outlook.com>
2026-03-20 10:21:39 +0300

    research stage 2 finished

--- feature_extractor.py
+++ feature_extractor.py
@@ -85,7 +85,6 @@ def process_audio_folder(folder_path, label=None): # перебор аудифо
     for file_name in os.listdir(folder_path):
         if file_name.endswith(('.wav', '.mp3', '.flac', '.m4a')):
             file_path = os.path.join(folder_path, file_name)
-            print(f'{counter} of {len(os.listdir(folder_path))}')
             counter += 1
             features = extract_features(file_path)
 
